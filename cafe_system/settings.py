63d95257bb350a2150f99cc26e6dec85e1e9be8c
Arby Sayid-Ibrakhimi <ingarbi006@gmail.com>
2025-04-28 17:20:58 +0300

    allowed host for all

--- cafe_system/settings.py
+++ cafe_system/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-090nkac(msy3q*u*0cy#ftf8d4a#=u4h0b-%)_tt=v4wfqxy-5"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     "daphne",
