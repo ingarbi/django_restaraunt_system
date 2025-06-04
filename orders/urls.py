2500b3da3a6b7ecd63d0a5535ab7174d4153f438
ingarbi <104501854+ingarbi@users.noreply.github.com>
2025-06-04 23:32:51 +0300

    quick print works

--- orders/urls.py
+++ orders/urls.py
@@ -1,19 +1,31 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('create/', views.create_order, name='create_order'),
-    path('kitchen/', views.kitchen_orders, name='kitchen_orders'),
-    path('complete/<int:order_id>/', views.mark_order_completed, name='mark_order_completed'),
-    path('delivere/<int:order_id>/', views.mark_order_delivered, name='mark_order_delivered'),
-    path('cancel/<int:order_id>/', views.mark_order_cancelled, name='mark_order_cancelled'),
+    path("create/", views.create_order, name="create_order"),
+    path("kitchen/", views.kitchen_orders, name="kitchen_orders"),
     path(
-        "order/<int:order_id>/pdf/", views.order_pdf, name="order_pdf"
+        "complete/<int:order_id>/",
+        views.mark_order_completed,
+        name="mark_order_completed",
     ),
     path(
-        "order/<int:order_id>/pdf/", views.order_pdf, name="quick_receipt_printing"
+        "delivere/<int:order_id>/",
+        views.mark_order_delivered,
+        name="mark_order_delivered",
     ),
-    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
-    path('orders/', views.all_orders, name='all_orders'),
+    path(
+        "cancel/<int:order_id>/",
+        views.mark_order_cancelled,
+        name="mark_order_cancelled",
+    ),
+    path("order/<int:order_id>/pdf/", views.order_pdf, name="order_pdf"),
+    path(
+        "order_quick/<int:order_id>/pdf/",
+        views.quick_receipt_printing,
+        name="quick_receipt_printing",
+    ),
+    path("order/<int:order_id>/", views.order_detail, name="order_detail"),
+    path("orders/", views.all_orders, name="all_orders"),
 ]
