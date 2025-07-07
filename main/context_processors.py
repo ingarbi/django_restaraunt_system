529150f1e41f7face4c74afc4a9a7e1d426fb898
Arby Sayid-Ibrakhimi <ingarbi006@gmail.com>
2025-07-07 10:55:11 +0300

    added context processor

---
+++ main/context_processors.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.http import HttpRequest
+from django.http import HttpResponse
+from django.shortcuts import render
+import os
+
+
+def cafe_name(request: HttpRequest) -> dict:
+    """Adds site-wide settings to the context."""
+    file_path = os.path.join(settings.BASE_DIR, "main/cafe_name.txt")
+
+    try:
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            return {
+                "CAFE_NAME": f"{file_content}",
+            }
+    except FileNotFoundError:
+        return {
+            "CAFE_NAME": f"{file_content}",
+        }
