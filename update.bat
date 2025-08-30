85d6951f0656a258d6ba5f59e5b3c84f16ee2c66
Arby Sayid-Ibrakhimi <ingarbi006@gmail.com>
2025-08-30 12:29:10 +0300

    update feature

---
+++ update.bat
@@ -0,0 +1,30 @@
+@echo off
+cd /d %~dp0
+
+echo ==========================
+echo Activating virtual environment...
+echo ==========================
+call venv\Scripts\activate
+
+echo ==========================
+echo Updating project...
+echo ==========================
+git pull origin main
+
+echo ==========================
+echo Installing dependencies...
+echo ==========================
+pip install -r requirements.txt
+
+echo ==========================
+echo Running migrations...
+echo ==========================
+python manage.py makemigrations
+python manage.py migrate
+
+echo ==========================
+echo Done! Project updated.
+echo This window will close automatically in 10 seconds...
+echo ==========================
+timeout /t 10 /nobreak >nul
+exit
