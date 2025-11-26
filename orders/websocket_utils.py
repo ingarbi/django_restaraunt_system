3bc0c550cbcbd028e64d02ce2e037c963bb6d070
ingarbi <104501854+ingarbi@users.noreply.github.com>
2025-11-26 20:37:27 +0300

    commented notification sound

--- orders/websocket_utils.py
+++ orders/websocket_utils.py
@@ -7,7 +7,7 @@ def send_order_update(order, message_type="order_update", action=None, extra_dat
     Send WebSocket update for order changes
     """
     channel_layer = get_channel_layer()
-    
+
     message_data = {
         "type": message_type,
         "order_id": order.id,
@@ -24,15 +24,15 @@ def send_order_update(order, message_type="order_update", action=None, extra_dat
                 {"name": item.menu_item.name, "quantity": item.quantity}
                 for item in order.items.all()
             ],
-        }
+        },
     }
-    
+
     if action:
         message_data["action"] = action
-    
+
     if extra_data:
         message_data.update(extra_data)
-    
+
     async_to_sync(channel_layer.group_send)("orders", message_data)
 
 
@@ -41,10 +41,10 @@ def send_new_order_notification(order):
     Send notification for a newly created order
     """
     send_order_update(
-        order, 
+        order,
         message_type="order_update",
         action="new_order",
-        extra_data={"message": "new_order"}
+        extra_data={"message": "new_order"},
     )
 
 
@@ -52,8 +52,4 @@ def send_order_status_change(order, action):
     """
     Send notification for order status changes
     """
-    send_order_update(
-        order,
-        message_type="order_update", 
-        action=action
-    )
+    send_order_update(order, message_type="order_update", action=action)
