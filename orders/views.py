854be248acd844c090b0f3807b5f4deab82aed81
unknown <ibragimbolkoev@yandex.ru>
2025-11-29 20:32:27 +0300

    Changing the display time period for orders in the All Orders section

--- orders/views.py
+++ orders/views.py
@@ -299,7 +299,7 @@ def all_orders(request):
     msk_tz = pytz.timezone("Europe/Moscow")
     now_msk = timezone.now().astimezone(msk_tz)
     #today = now_msk.date()
-    twenty_eight_hours_ago = now_msk - timedelta(hours=27)
+    twenty_eight_hours_ago = now_msk - timedelta(hours=4)
 
     # Filter orders created today and sort by status
     orders = Order.objects.filter(created_at__gte=twenty_eight_hours_ago).order_by(
